aafe208c241010960d40ee06fd8771e3d3fc9b18
yudonglin <37920527+yudonglin@users.noreply.github.com>
2021-04-04 21:00:38 -0700

    3.0.3 release

--- __init__.py
+++ __init__.py
@@ -1,11 +1,9 @@
-# cython: language_level=3
 #加载Zero引擎的必要组件
-from .scr_py.dialog import *
+from .dialog import *
 try:
-    from .scr_py.battle import *
+    from .battle import *
 except FileNotFoundError:
     print("Battle system is disabled because some files are missing.")
-from .scr_core.tool import *
 import platform
 
 print("linpg {0} (pygame {1}, python {2})".format(get_current_version(),pygame.version.ver,platform.python_version()))
